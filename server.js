6c13f486dee4c71c85642ace363c4cdcb17b6235
Sepehr Fard <sardebil@ucsd.edu>
2020-11-09 14:54:22 -0800

    connected mongodb

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+
+const dbUrl = 'mongodb://localhost/zipdb';
+MongoClient.connect(dbUrl, function (err, db) {
+    if (err) throw err;
+    console.log("DB Connected");
+    db.close();
+})
+
 const app = express();
 const port = process.env.PORT || 3000
 
